ea003794dba996d7085186dfdb46ed16dfe79dea
HeroBrian389 <briankelleher38@gmail.com>
2022-10-08 15:50:16 +0100

    update

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     // basic site info (required)
     name: "Brian Kelleher",
-    domain: 'jackoregankenny.com',
+    domain: 'notionsite2-xi.vercel.app',
     author: "Brian Kelleher",
 
     // open graph metadata (optional)
